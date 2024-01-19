75d611bc7f8434b690b7720900b0c54925add0e2
David <david@midnightmechanism.com>
2024-01-19 16:47:42 -0600

    Fix female data

--- scripts/format-hist-mort.py
+++ scripts/format-hist-mort.py
@@ -8,7 +8,7 @@ import pandas as pd
 import pandas as pd
 
 male_data = pd.read_csv("../storage/app/data/berkley-mortality-database/male-mortality-1941-2020.csv")
-female_data = pd.read_csv("../storage/app/data/berkley-mortality-database/male-mortality-1941-2020.csv")
+female_data = pd.read_csv("../storage/app/data/berkley-mortality-database/female-mortality-1941-2020.csv")
 
 # Take year, age, age spec. mort., number of lives, and life expectancy
 male_subset = male_data[['Year', 'Age', 'mx', 'lx', 'ex']]
